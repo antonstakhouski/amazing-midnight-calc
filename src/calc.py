3c01ed1b1d50e97973fde9b8fce1099e1be73f37
Anton Stakhouski 450502 <anton.stakhouski@gmail.com>
2016-12-29 23:07:30 +0300

    first function was added

--- src/calc.py
+++ src/calc.py
@@ -1,17 +1,31 @@
-OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
-             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+import string
+from math import *
+
+BINARY_OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+                    '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+FUNCTIONS = {'sqr': (9, lambda x: x * x)}
+alphabet = list(string.ascii_lowercase)
 
 
 def eval_(formula):
     def parse(formula_string):
         number = ''
+        function = ''
         for s in formula_string:
             if s in '1234567890.':
                 number += s
             elif number:
                 yield float(number)
                 number = ''
-            if s in OPERATORS or s in "()":
+
+            if s in alphabet:
+                function += s
+            elif function:
+                # print(function)
+                yield function
+                function = ''
+
+            if s in BINARY_OPERATORS or s in "()":
                 yield s
         if number:
             yield float(number)
@@ -19,8 +33,12 @@ def eval_(formula):
     def shunting_yard(parsed_formula):
         stack = []
         for token in parsed_formula:
-            if token in OPERATORS:
-                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+            if token in FUNCTIONS:
+                while stack and stack[-1] != "(" and FUNCTIONS[token][0] <= FUNCTIONS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token in BINARY_OPERATORS:
+                while stack and stack[-1] != "(" and BINARY_OPERATORS[token][0] <= BINARY_OPERATORS[stack[-1]][0]:
                     yield stack.pop()
                 stack.append(token)
             elif token == ")":
@@ -39,9 +57,12 @@ def eval_(formula):
     def calc(polish):
         stack = []
         for token in polish:
-            if token in OPERATORS:
+            if token in BINARY_OPERATORS:
                 y, x = stack.pop(), stack.pop()
-                stack.append(OPERATORS[token][1](x, y))
+                stack.append(BINARY_OPERATORS[token][1](x, y))
+            elif token in FUNCTIONS:
+                x = stack.pop()
+                stack.append(FUNCTIONS[token][1](x))
             else:
                 stack.append(token)
         return stack[0]
--- tests/calc_test.py
+++ tests/calc_test.py
@@ -9,9 +9,18 @@ class CalcTestSequence(unittest.TestCase):
         formula = "(2+2*2)/(2+2*2)-5"
         self.assertEqual(-4.0, eval_(formula))
 
+    def test_sqr(self):
+        formula = "sqr(2)"
+        self.assertEqual(4.0, eval_(formula))
+
     def test_epam_easy(self):
-        formula = "1*4+3.3/(3 + .3)*3(sqrt(4))/(sin(0) + 1)"
+        # formula = "1*4+3.3/(3 + .3)*3(sqrt(4))/(sin(0) + 1)"
+        formula = "1*4+3.3/(3 + .3)*3*(sqrt(4))/(sin(0) + 1)"
         self.assertEqual(10, eval_(formula))
+
+simpleTestSuite = unittest.TestSuite()
+simpleTestSuite.addTest(CalcTestSequence('test_sumMulDivSub'))
+simpleTestSuite.addTest(CalcTestSequence('test_sqr'))
 
 if __name__ == '__main__':
     unittest.main()
